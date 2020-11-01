7ee3db233ed41365b85041e95c9160cafa1b201e
Pirky <m.pirkovsky@gmail.com>
2020-11-01 21:09:10 +0100

    pridany crossover, tournament

--- program/hladac_pokladov.py
+++ program/hladac_pokladov.py
@@ -1,6 +1,8 @@
 import random
 
+random.seed(0)
 
+NUM_OF_CELLS = 32
 map_lines = 0
 map_columns = 0
 all_tresures = {}
@@ -32,22 +34,22 @@ def first_generation(num_of_individuals):
         individuals[i]["fitness"] = 0
         individuals[i]["path"] = []
         individuals[i]["memory_cells"] = [0 for _ in range(64)]
-        individuals[i]["treasures"] = 0
-        for j in range(25):                              # prvych 25 byteov bude random
+        individuals[i]["treasures"] = list()
+        for j in range(NUM_OF_CELLS):
             individuals[i]["memory_cells"][j] = random.randint(0, 255)
     return individuals
 
 
 def write(move):
     if move == "00":
-        move = "H"
-    if move == "01":
-        move = "D"
-    if move == "10":
-        move = "P"
-    if move == "11":
-        move = "L"
-    return move
+        return "H"
+    elif move == "01":
+        return "D"
+    elif move == "10":
+        return "P"
+    elif move == "11":
+        return "L"
+    return None
 
 
 def virtual_machine(individual):
@@ -58,20 +60,20 @@ def virtual_machine(individual):
         operation = format(instructions[index], '#010b')[2:4]
         address = int(format(instructions[index], '#010b')[4:], 2)
 
-        if operation == "00":       # increment
+        if operation == "00":  # increment
             instructions[address] += 1
             if instructions[address] == 256:
                 instructions[address] = 0
 
-        elif operation == "01":     # decrement
+        elif operation == "01":  # decrement
             instructions[address] -= 1
             if instructions[address] == -1:
                 instructions[address] = 255
 
-        elif operation == "10":     # jump
+        elif operation == "10":  # jump
             index = address
 
-        elif operation == "11":     # write uses last 2 bits
+        elif operation == "11":  # write uses last 2 bits
             move = format(instructions[index], '#010b')[8:]
             individual["path"].append(write(move))
 
@@ -79,15 +81,124 @@ def virtual_machine(individual):
         index += 1
 
 
+def found_treasures(individual):
+    global all_tresures
+    global start_line
+    global start_column
+    global map_lines
+    global map_columns
+
+    curr_line = start_line
+    curr_column = start_column
+    counter = 0
+    for move in individual["path"]:
+        if move == "H":
+            curr_line -= 1
+        elif move == "D":
+            curr_line += 1
+        elif move == "P":
+            curr_column += 1
+        elif move == "L":
+            curr_column -= 1
+
+        if not 0 <= curr_line < map_lines or not 0 <= curr_column < map_columns:  # check if I'm in the map
+            individual["path"] = individual["path"][:counter]
+            return
+
+        curr_pos = [curr_line, curr_column]
+        if curr_pos in all_tresures.values() and curr_pos not in individual["treasures"]:  # check for treasure
+            individual["treasures"].append(curr_pos)
+
+        if len(individual["treasures"]) == len(all_tresures):
+            print("All treasures were found")
+            return
+        counter += 1
+
+
+def set_fitness(individual):
+    if len(individual["path"]) == 0:
+        return
+
+    individual["fitness"] = len(individual["treasures"]) + 1 - len(individual["path"]) / 1000
+
+
+def fresh_individual():
+    individual = {"fitness": 0, "path": [], "memory_cells": [0 for _ in range(64)], "treasures": list()}
+    for i in range(NUM_OF_CELLS):
+        individual["memory_cells"][i] = random.randint(0, 255)
+    return individual
+
+
+def crossover(mom, dad):
+    child = []
+    copy_length = random.randint(0, 64)
+    start_index = random.randint(0, 64 - copy_length)
+
+    for i in range(64):
+        if start_index <= i < start_index + copy_length:
+            child.append(mom[i])
+        else:
+            child.append(dad[i])
+
+    individual = {"fitness": 0, "path": [], "memory_cells": child, "treasures": list()}
+    return individual
+
+
+def tournament(generation, new_generation):
+    for i in range(2, 89):
+        sorted_individuals = sorted(random.choices(generation, k=20), reverse=True, key=lambda x: x["fitness"])
+        new_generation[i] = crossover(sorted_individuals[0]["memory_cells"], sorted_individuals[1]["memory_cells"])
+
+    return new_generation
+
+
+def create_generation(generation):
+    new_generation = {}
+    sorted_gen = [i[1] for i in sorted(generation.items(), reverse=True, key=lambda x: x[1]["fitness"])]
+    for i in range(2):  # best 2 are going to next generation without change
+        new_generation[i] = sorted_gen[i]
+
+    tournament(generation, new_generation)
+
+    for i in range(89, 100):
+        new_generation[i] = fresh_individual()
+
+    # este mutovanie dorobit
+    return new_generation
+
+
+def info_generation(generation):
+    sorted_gen = [i[1] for i in sorted(generation.items(), reverse=True, key=lambda x: x[1]["fitness"])]
+    if sorted_gen[0]["fitness"] >= 5:
+        return True
+    print(f"""Best individual info:
+    fitness:        {sorted_gen[0]["fitness"]}
+    path:           {sorted_gen[0]["path"]}
+    path length:    {len(sorted_gen[0]["path"])}""")
+    return False
+
+
 def start():
     num_of_generations = 1
-    num_of_individuals = 50
+    num_of_individuals = 100
     init()
     generation = first_generation(num_of_individuals)
     for i in range(num_of_generations):
+        print(f"{i}. generation")
         for curr_ind in range(num_of_individuals):
             print(f"{curr_ind}. individual")
             virtual_machine(generation[curr_ind])
+            found_treasures(generation[curr_ind])
+            set_fitness(generation[curr_ind])
+
+        done = info_generation(generation)
+        if done:
+            print("Finito. Found solution.")
+            return
+        generation = create_generation(generation)
 
 
 start()
+
+
+# sorted_gen = [i[0] for i in sorted(generation.items(), reverse=True, key=lambda x: x[1]["fitness"])]
