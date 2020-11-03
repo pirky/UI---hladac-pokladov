996bee8862c6f89c9bfc72736e0f44c2714dfeda
Pirky <m.pirkovsky@gmail.com>
2020-11-03 15:56:59 +0100

    upravene pomery a urobene mutacie

--- program/hladac_pokladov.py
+++ program/hladac_pokladov.py
@@ -4,17 +4,17 @@ random.seed(0)
 
 NUM_OF_CELLS = 32           # number of cells initialized in first generation
 ELITISM = 0.02              # percent of new individuals made by elitism
-FRESH = 0.1                 # percent of new individuals made for new generation
-CROSSOVER = 0.6             # percent of new individuals made by crossover
+FRESH = 0.2                 # percent of new individuals made for new generation
+CROSSOVER = 0.7             # percent of new individuals made by crossover
 TOURNAMENT = 0.5            # percent of individuals in tournament
-
+MUTATION = 0.2              # percent of best individuals chosen to choose from in mutation
 
 NUM_OF_GENERATIONS = 100
 NUM_OF_INDIVIDUALS = 400
 
 map_lines = 0
 map_columns = 0
-all_tresures = {}
+all_treasures = {}
 start_line = 0
 start_column = 0
 
@@ -22,7 +22,7 @@ start_column = 0
 def init(file_path):
     global map_lines
     global map_columns
-    global all_tresures
+    global all_treasures
     global start_line
     global start_column
     file = open(file_path, "r")
@@ -41,7 +41,7 @@ def init(file_path):
 
     for i in range(data["num_of_treasures"]):
         treasure = f"treasure{i}"
-        all_tresures[i] = [int(data[treasure][0]), int(data[treasure][1][:1])]
+        all_treasures[i] = [int(data[treasure][0]), int(data[treasure][1][:1])]
 
     file.close()
 
@@ -104,7 +104,7 @@ def virtual_machine(individual):
 
 
 def found_treasures(individual):
-    global all_tresures
+    global all_treasures
     global start_line
     global start_column
     global map_lines
@@ -128,10 +128,10 @@ def found_treasures(individual):
             return
 
         curr_pos = [curr_line, curr_column]
-        if curr_pos in all_tresures.values() and curr_pos not in individual["treasures"]:  # check for treasure
+        if curr_pos in all_treasures.values() and curr_pos not in individual["treasures"]:  # check for treasure
             individual["treasures"].append(curr_pos)
 
-        if len(individual["treasures"]) == len(all_tresures):
+        if len(individual["treasures"]) == len(all_treasures):
             # print("All treasures were found")
             return
         counter += 1
@@ -166,23 +166,22 @@ def crossover(mom, dad):
     return individual
 
 
-def tournament(generation, new_generation):
+def tournament(sorted_gen, new_generation):
     start_index = int((ELITISM + FRESH) * NUM_OF_INDIVIDUALS)
     end_index = int((ELITISM + FRESH + CROSSOVER) * NUM_OF_INDIVIDUALS)
     num_tournament = int(TOURNAMENT * NUM_OF_INDIVIDUALS)
 
     for i in range(start_index, end_index):
-        sorted_individuals = sorted(random.choices(generation, k=num_tournament), reverse=True, key=lambda x: x["fitness"])
+        sorted_individuals = sorted(random.choices(sorted_gen, k=num_tournament), reverse=True, key=lambda x: x["fitness"])
         new_generation[i] = crossover(sorted_individuals[0]["memory_cells"], sorted_individuals[1]["memory_cells"])
-
-    return new_generation
 
 
 def mutate_little(individual):
     for i in range(2):
         index = random.randint(0, 63)
         individual["memory_cells"][index] = random.randint(0, 255)
-    return individual
+
+    return {"fitness": 0, "path": [], "memory_cells": individual["memory_cells"], "treasures": list()}
 
 
 def mutate_random(individual):
@@ -190,76 +189,73 @@ def mutate_random(individual):
     for i in range(mut_count):
         index = random.randint(0, 63)
         individual["memory_cells"][index] = random.randint(0, 255)
-    return individual
+
+    return {"fitness": 0, "path": [], "memory_cells": individual["memory_cells"], "treasures": list()}
 
 
 def mutate_switch(individual):
-    index_1 = random.randint(0, 60)
-    index_2 = random.randint(0, 60)
+    blok_length = 6
+    index_1 = random.randint(0, 63 - blok_length)
+    index_2 = random.randint(0, 63 - blok_length)
 
-    while abs(index_1 - index_2) < 3:
-        index_1 = random.randint(0, 60)
-        index_2 = random.randint(0, 60)
+    while abs(index_1 - index_2) < blok_length:
+        index_1 = random.randint(0, 63 - blok_length)
+        index_2 = random.randint(0, 63 - blok_length)
 
     minimum = min(index_1, index_2)
-    copy_1 = individual["memory_cells"][minimum: minimum + 3]
+    copy_1 = individual["memory_cells"][minimum: minimum + blok_length]
     maximum = max(index_1, index_2)
-    copy_2 = individual["memory_cells"][maximum: maximum + 3]
+    copy_2 = individual["memory_cells"][maximum: maximum + blok_length]
 
-    for i in range(minimum, maximum + 4):
-        if i <= minimum + 3:
+    for i in range(minimum, maximum + blok_length):
+        if i < minimum + blok_length:
             individual["memory_cells"][i] = copy_2.pop()
         elif i >= maximum:
             individual["memory_cells"][i] = copy_1.pop()
 
-    return individual
+    return {"fitness": 0, "path": [], "memory_cells": individual["memory_cells"], "treasures": list()}
 
 
 def mutation_selection(sorted_gen, new_generation):
-    index = int((1 - (ELITISM + FRESH + CROSSOVER)) * NUM_OF_INDIVIDUALS)
+    index = int(MUTATION * NUM_OF_INDIVIDUALS)
     best_individuals = sorted_gen[:index]
     index = int((ELITISM + FRESH + CROSSOVER) * NUM_OF_INDIVIDUALS)
-    # dorobit mutacia najlepsich je pridane ako posledna caast novej generacie
+
     for i in range(index, NUM_OF_INDIVIDUALS):
-        individual = random.choice(best_individuals.items())
+        individual = random.choice(best_individuals)
         new_generation[i] = mutate_switch(individual)
 
 
-def create_generation(generation):
+def create_generation(sorted_gen):
     new_generation = {}
-    sorted_gen = [i[1] for i in sorted(generation.items(), reverse=True, key=lambda x: x[1]["fitness"])]
-    # elitism
-    elite_end = int(ELITISM * NUM_OF_INDIVIDUALS)
+
+    elite_end = int(ELITISM * NUM_OF_INDIVIDUALS)       # elitism
     for i in range(0, elite_end):
-        new_generation[i] = sorted_gen[i]
-    # fresh individuals
-    start_fresh = int(ELITISM * NUM_OF_INDIVIDUALS)
+        new_generation[i] = {"fitness": 0, "path": [], "memory_cells": sorted_gen[i]["memory_cells"], "treasures": list()}
+
+    start_fresh = int(ELITISM * NUM_OF_INDIVIDUALS)     # fresh individuals
     end_fresh = int((ELITISM + FRESH) * NUM_OF_INDIVIDUALS)
     for i in range(start_fresh, end_fresh):
         new_generation[i] = fresh_individual()
-    # dorobit tournament, crossover iba najlepsich 50 napr
-    tournament(generation, new_generation)
-    # dorobit mutaciu najlepsich
-    start_mutation = int((ELITISM + CROSSOVER + FRESH) * NUM_OF_INDIVIDUALS)
-    for _ in range(NUM_OF_INDIVIDUALS - start_mutation):
-        index = random.randint(start_mutation, NUM_OF_INDIVIDUALS - 1)
-        new_generation[index] = mutate_random(new_generation[index])
+
+    tournament(sorted_gen, new_generation)              # crossover
+
+    mutation_selection(sorted_gen, new_generation)      # mutation
 
     return new_generation
 
 
-def info_generation(generation):
-    sorted_gen = [i[1] for i in sorted(generation.items(), reverse=True, key=lambda x: x[1]["fitness"])]
+def info_generation(sorted_gen):
     # if sorted_gen[0]["fitness"] >= 5:
     #     return True
-    #
-    avg = round(sum([i["fitness"] for i in generation.values()]) / NUM_OF_INDIVIDUALS, 3)
-    # print(avg)
-    print(f"Generation avg fitness: {avg}")
-    print(f"""Best individual info:
-    fitness:        {sorted_gen[0]["fitness"]}
-    path:           {sorted_gen[0]["path"]}
-    path length:    {len(sorted_gen[0]["path"])}""")
+
+    avg = round(sum([i["fitness"] for i in sorted_gen]) / NUM_OF_INDIVIDUALS, 3)
+    print(f"avg:\t{avg}", "\tbest:\t{}".format(sorted_gen[0]["fitness"]))
+    # print(f"Generation avg fitness: {avg}")
+    # print(f"""Best individual info:
+    # fitness:        {sorted_gen[0]["fitness"]}
+    # path:           {sorted_gen[0]["path"]}
+    # path length:    {len(sorted_gen[0]["path"])}""")
 
     return False
 
@@ -277,16 +273,11 @@ def start():
             found_treasures(generation[curr_ind])
             set_fitness(generation[curr_ind])
 
+        sorted_gen = [i[1] for i in sorted(generation.items(), reverse=True, key=lambda x: x[1]["fitness"])]
         print(f"{i}. generation")
-        info_generation(generation)
-        # if i % 10 == 0:
-        #     print(f"{i}. generation")
-        #     done = info_generation(generation)
-        #     if done:
-        #         print("Finito. Found solution.")
-        #         return
+        info_generation(sorted_gen)
 
-        generation = create_generation(generation)
+        generation = create_generation(sorted_gen)
 
 
 start()
